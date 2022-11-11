cb0f20cc7b17e4feb7130e8d396d7336427bcdbf
Alex <alexjooho@gmail.com>
2022-11-11 01:52:28 -0800

    worked on question 141

---
+++ linked-list/141_linked_list_cycle.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # create a set for visited nodes
+        # traverse the linked list and add nodes as they are visited
+        # if the list reaches the end without seeing a duplicate, return False
+        # if there is a duplicate, return True
+
+        seen = set()
+        curr = head
+
+        while curr:
+            if curr in seen:
+                return True
+            else:
+                seen.add(curr)
+            curr = curr.next
+        
+        return False
+        
+# neetcode solution: (tortoise and hare)
+
+# class Solution:
+#     def hasCycle(self, head: ListNode) -> bool:
+#         slow, fast = head, head
+
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#             if slow == fast:
+#                 return True
+#         return False
